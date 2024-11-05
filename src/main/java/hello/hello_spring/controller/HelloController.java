2a43fd6e4771968fe11f8789cf76e57333858614
MinSung Jung <minpro22@naver.com>
2024-11-05 17:03:48 +0900

    변경

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,51 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // Spring은 컨트롤러 만들때 해줘야됨.
+public class HelloController {
+
+    @GetMapping("hello") // /hello가 들어오면, 이 메서드를 호출시켜줌.
+    public String hello(Model model){ // MVC의 model임.
+        model.addAttribute("data","hello!!"); // 키, 속성
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body에 이 데이터의 내용을 직접 넣어준다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring" 위와 다르게, html 태그 없이 내용만 나옴.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // hello라는 객체를 넘김 -> JSON 방식으로 html이 나옴. key:value, Spring은 JSON으로 반환하는게 디폴트임.
+
+    }
+
+    static class Hello{
+        private String name;
+        // 프로퍼티 방식 getter setter
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+}
---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //id 셋팅
+        store.put(member.getId(), member);//store 맵에 멤버의 아이디와 멤버의 이름 저장
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //Optional.ofNullable: id의 값이 없으면, null 반환해줌.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(memeber -> member.getName().equals(name)) //람다식
+                .findAny(); //람다식으로 루프를 돌아서 이름 값이 같으면, 이름을 반환하고, 없으면 Optional의 null이 포함돼서 반환.
+    }
+
+    @Override
+    public ArrayList<Member> findAll() {
+        return new ArrayList<>(store.values()); //member들이 반환됨.
+    }
+}
